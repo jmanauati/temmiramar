0c5c212c18b002b3ef9d2c4585ff5ceff667b342
JAnauati <jmanauati@gmail.com>
2024-09-10 18:41:42 +0200

    listo

--- src/componentes/Item.jsx
+++ src/componentes/Item.jsx
@@ -6,7 +6,7 @@ function Item({producto}) {
       <img src={producto.iamge} className='item-img'/> 
       <div className='item-data'>
         <h4 className='item-title'>{producto.title}</h4>
-        <p className='item-price'>${producto.price}</p>
+        <p className='item-price'>{producto.price}</p>
         <p className='item-description'>{producto.description}</p>
         <a href={`temmiramar/#/contacto`}className='ver-mas'>CONSULTAR</a>
       </div>
